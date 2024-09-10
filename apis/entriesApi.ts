b4e5e83cd980d485c51c62af7eb9e9f6c1c3c2c0
alexander <castro.t.alex@gmail.com>
2024-09-09 19:27:16 -0500

    changing the api gateway url

--- apis/entriesApi.ts
+++ apis/entriesApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // como la api sale del mismo dominio no es necesario poner la url completa http://localhost:3000/api/entries
 const entriesApi= axios.create({
-    baseURL: "https://w80sf88mec.execute-api.us-east-1.amazonaws.com",
+    baseURL: "https://z8jpaiv8m8.execute-api.us-east-1.amazonaws.com/",
    
 });
 
